5217b867f91c2aedf6eb284bd4f705c4dae3009a
LavaCubeL <danielferguson038@gmail.com>
2022-09-19 21:30:28 -0400

    added changes to design

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ myHeaders.append("Accept", "application/json");
 let button = document.getElementById('button-style')
 let BoxField = document.getElementById('FactBox')
 
-
-
+let myCheck = document.getElementById("testCheck")
+let colorTxt = document.getElementById('txtColor')
 
 var requestOptions = {
   method: 'GET',
@@ -12,19 +12,28 @@ var requestOptions = {
   redirect: 'follow'
 };
 
-
+var changednum = 140
+let urlTest = `https://catfact.ninja/fact?max_length=${changednum}`
 
 
 
 
 
   function test(){
-    fetch("https://catfact.ninja/fact?max_length=140", requestOptions)
+    BoxField.style.color = colorTxt.value
+      console.log('it works')
+       // console.log(changednum)
+    
+     fetch(urlTest, requestOptions)
   .then(response => response.text())
 //    .then(result => console.log(result))
-    .then(result => BoxField.setAttribute('value',result))
+     .then(result => BoxField.setAttribute('value',result))
   
   .catch(error => console.log('error', error));
 //   console.log(BoxField.nodeValue)
+
 }
 button.addEventListener('click',test)
+
+// Last test of skill make a changable value in the api
+// template string it andd send you ew
