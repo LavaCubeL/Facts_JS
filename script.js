f0b2bd6e72bdc9342e01e1fc21f1fa95a1b018fb
LavaCubeL <danielferguson038@gmail.com>
2022-09-22 19:50:48 -0400

    Final design for project

--- script.js
+++ script.js
@@ -19,21 +19,17 @@ let urlTest = `https://catfact.ninja/fact?max_length=${changednum}`
 
 
 
-  function test(){
+  async function test(){
     BoxField.style.color = colorTxt.value
-      console.log('it works')
-       // console.log(changednum)
+     
+      
     
-     fetch(urlTest, requestOptions)
-  .then(response => response.text())
-//    .then(result => console.log(result))
-     .then(result => BoxField.setAttribute('value',result))
+      fetch(urlTest, requestOptions)
+   .then( response =>  response.text()
+   ).then(result => BoxField.setAttribute('value', result))
   
-  .catch(error => console.log('error', error));
-//   console.log(BoxField.nodeValue)
+  
 
 }
 button.addEventListener('click',test)
 
-// Last test of skill make a changable value in the api
-// template string it andd send you ew
